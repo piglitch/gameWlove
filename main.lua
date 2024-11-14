8066fa8a5106671eaaaba072ed2dde178f931ca4
Avi <avibanerjee2000@gmail.com>
2024-11-14 21:29:06 +0530

    trying out movements

--- main.lua
+++ main.lua
@@ -3,6 +3,8 @@ function love.load ()
   target.x = 300
   target.y = 300
   target.radius = 50
+  target.speed = 50
+  target.state = false
 
   score = 0
   timer = 0
@@ -11,12 +13,23 @@ function love.load ()
 end
 
 function love.update(dt)
-  -- number = number + 1
+    if target.state == true then
+      -- score = score + 1
+      target.x = target.x + target.speed * dt
+      target.y = target.y + target.speed * dt
+    end
 end
 
 function love.draw()
-
-
+  function love.mousepressed(x, y, button, isTouch, presses)
+    
+    if button == 1  then
+      mouseToTarget = distanceBw(x, y, target.x, target.y)
+      if mouseToTarget < target.radius then
+        target.state = not target.state
+      end
+    end
+  end
   love.graphics.setColor(1, 0.2, 0.2)
   love.graphics.circle("fill", target.x, target.y, target.radius)
 
@@ -25,16 +38,7 @@ function love.draw()
   love.graphics.print(score, 0, 0)
 end
 
-function love.mousepressed(x, y, button, isTouch, presses)
-  if button == 1 then
-    local mouseToTarget = distanceBw(x, y, target.x, target.y)
-    if mouseToTarget < target.radius then
-      score = score + 1
-      target.x = 500
-      targte.y = 400
-    end
-  end
-end
+
 
 function distanceBw(x1, y1, x2, y2)
   return math.sqrt((x2-x1)^2 + (y2-y1)^2)
